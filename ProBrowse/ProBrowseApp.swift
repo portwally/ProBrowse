1b77934b9912bcec86959f9aa9e1bbdd84acbc08
portwally <walter.tengler@gmail.com>
2025-12-23 08:38:48 +0000

    Initial Commit

---
+++ ProBrowse/ProBrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProBrowseApp.swift
+//  ProBrowse
+//
+//  Created by Walter Tengler on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProBrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
